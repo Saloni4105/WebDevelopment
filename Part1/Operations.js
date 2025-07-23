133479e41590403aac7580f677e3fe3ee5371da3
Saloni4105 <salonigawande04@gmail.com>
2025-07-23 11:01:17 +0530

    JS foundation

---
+++ Part1/Operations.js
@@ -0,0 +1,31 @@
+let score= 102;
+let bonus=25
+
+let totalScore= score + bonus;
+
+// Arithmetic Operations
+let addition= 4 + 3;
+let Substraction= 9 - 8;
+let mul= 4 * 3;
+let div= 4 / 2;
+let rem= 5 % 2;
+let expo= 2**5; //exponential, power of anything
+console.log(rem);
+console.log(expo);
+
+
+let myscore= 100;
+myscore++; // Increment
+console.log(myscore);
+
+let credits= 24;
+credits--; //Decrement
+
+// Comparison Operator
+let num1=8;
+let num2=8;
+let num3=5;
+
+console.log(num1==num2);
+console.log(num1!=num3);
+console.log(num1 < num3);
